21d7af70c83646bf804d5fc98d75267fff2e0e97
Maria Clara Fagundes <109442107+Mariacfagundes@users.noreply.github.com>
2026-01-08 16:34:31 -0300

    Update app.py

--- notbook/streamlit_app/app.py
+++ notbook/streamlit_app/app.py
@@ -43,15 +43,25 @@ for name, df in dfs.items():
 def detect_key(df, keys):
     cols = df.columns.str.lower()
     for k in keys:
-        if k.lower() in cols.values:
+        if k in cols.values:
             return df.columns[list(cols).index(k)]
     return None
 
-# Pega tabelas
 fact = dfs["fact_sales"]
 dim_prod = dfs["dim_product"]
 dim_cat = dfs["dim_categories"]
 dim_cal = dfs["dim_calendar"]
 
-# Detecta chaves
-product_key_fact = detect_key(fact, ["product", "product_id", "productkey", "product_key", "prod]()_
+# listas SEM quebra
+product_keys = ["product", "product_id", "productkey", "product_key", "prod_id", "sku"]
+category_keys = ["category", "category_id", "categorykey", "category_key", "cat_id"]
+date_keys = ["date", "date_id", "datekey", "date_key", "time_id"]
+
+product_key_fact = detect_key(fact, product_keys)
+product_key_dim = detect_key(dim_prod, product_keys)
+
+category_key_fact = detect_key(fact, category_keys)
+category_key_dim = detect_key(dim_cat, category_keys)
+
+date_key_fact = detect_key(fact, date_keys)
+date_key_dim = detect_key(dim_cal, date_keys)
